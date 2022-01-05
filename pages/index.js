21ea2385b1eda851c1eaddf639a9cde941cb9764
clxmente <clem@csu.fullerton.edu>
2022-01-05 01:42:23 -0800

    mobile fix, word break fix

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ export default function Home() {
 
       <main>
         <div className="flex justify-center items-center h-screen">
-          <div>
+          <div className="inline-block justify-center">
             {/* Nav Bar Code */}
-            <div className="h-24 flex justify-evenly sm:justify-start">
+            <div className="h-24 flex justify-start px-5 sm:px-0">
               <nav className="font-mono text-lg text-gray-200 no-underline">
                 <div className="flex">
                   <div className="pr-6"><Link href={"/"}><a className="hover:text-mygreen-500">/home</a></Link></div>
@@ -28,22 +28,20 @@ export default function Home() {
               </nav>
             </div>
             {/* END Nav Bar */}
-            <h1 className="font-mono text-gray-200 font-bold text-4xl sm:text-6xl flex justify-evenly sm:justify-start">
+            <h1 className="font-mono text-gray-200 font-bold text-4xl sm:text-6xl flex justify-start px-5 sm:px-0">
               hello, i'm clem.
             </h1>
-            <div className='px-12 sm:px-0'>
+            <div className='px-5 sm:px-0 w-full'>
               <p className="font-mono text-neutral-500 text-lg leading-8 pt-8 sm:px-0">
-                I'm Clemente Solorio, a 20 year old student enrolled{" "}
-              </p>
-              <p className="font-mono text-neutral-500 text-lg sm:px-0">
+                I'm Clemente Solorio, a 20 year old student enrolled <br className='hidden sm:block'/>
                 <a 
                   className="text-mygreen-100 hover:text-mygreen-500 hover:cursor-pointer"
                   href="https://www.fullerton.edu/">
                   @CSUF
-                </a>, majoring in Computer Science.
+                </a>. I'm currently in my 3rd year of university,<br className='hidden sm:block'/>completing my Bachelors of Science in Computer Science.
               </p>
             </div>
-            <div className="flex flex-nowrap pt-8 space-x-8 justify-evenly sm:justify-start">
+            <div className="flex flex-nowrap pt-8 space-x-8 justify-start px-5 sm:px-0">
               <Social href={"https://github.com/clxmente"} icon={<AiOutlineGithub size={"36"} />} />
               <Social href={"https://www.linkedin.com/in/clementesolorio/"} icon={<BsLinkedin size={"36"} />} />
               <Social href={"mailto:clem@solorio.dev"} icon={<IoMdMail size={"36"} />} />
@@ -51,9 +49,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
-        <p>
-        </p>
       </main>
     </div>
   )
