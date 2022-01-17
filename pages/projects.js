b3ecdd75e09838872eedadfc132841f8af77df49
clxmente <clem@csu.fullerton.edu>
2022-01-17 15:11:40 -0800

    projects card links

--- pages/projects.js
+++ pages/projects.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from "next/link";
 import { Navbar } from "../components/Navbar";
 import { FaPython, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFastapi } from "react-icons/si";
+import { SiTailwindcss, SiFastapi, SiMongodb, SiNginx } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { DiPhp, DiHtml5, DiCss3 } from "react-icons/di";
 
@@ -14,7 +14,7 @@ function Projects() {
           <meta name="description" content="Page with some of the projects I have worked on." />
           <link rel="icon" href="https://github.com/clxmente.png"/>
         </Head>
-  
+
         <main>
           <div className="flex justify-center items-center sm:h-screen overflow-auto">
             <div className="inline-block justify-center w-4/5 2xl:w-1/2 pt-12 sm:pt-0">
@@ -24,14 +24,23 @@ function Projects() {
               <h1 className="font-mono text-gray-200 font-bold text-3xl sm:text-4xl flex justify-start">
                 projects
               </h1>
+              {/* PW Manager */}
               <div className="py-10 grid grid-cols-1 xl:grid-cols-2 gap-8"> {/* grid container */}
-                <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
-                <div className="flex items-center justify-between">
-                  <p className='font-bold text-lg font-mono text-gray-200'>Python Password Manager</p>
-                  <FaPython size={"20"} color={"white"}/>
-                </div>
-                  <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>command-line password manager that stores encrypted passwords.</p>
-                </div>
+                {/* PW Manager */}
+                <Link href={"https://github.com/clxmente/Python-Password-Manager"}>
+                  <a>
+                  <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
+                  <div className="flex items-center justify-between">
+                    <p className='font-bold text-lg font-mono text-gray-200'>Python Password Manager</p>
+                    <FaPython size={"20"} color={"white"}/>
+                  </div>
+                    <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>command-line password manager that stores encrypted passwords.</p>
+                  </div>
+                  </a>
+                </Link>
+                {/* END PW Manager */}
+                {/* University DB */}
+                {/* NOT allowed to upload code or proj to internet bc it was for a class. */}
                 <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
                   <div className="flex items-center justify-between">
                     <p className='font-bold text-lg font-mono text-gray-200'>University Database</p>
@@ -44,26 +53,40 @@ function Projects() {
                   </div>
                   <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>designed a university DB and user interface using MySQL, PHP, HTML, and CSS.</p>
                 </div>
-                <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
-                  <div className="flex items-center justify-between">
-                    <p className='font-bold text-lg font-mono text-gray-200'>NBA Players API</p>
-                    <div className="flex items-center">
-                      <div className="pr-4 sm:pr-7"><SiFastapi size={"20"} color="white" /></div>
-                      <FaPython size={"20"} color={"white"}/>
+                {/* END University DB */}
+                {/* NBA Players API */}
+                <Link href={"https://github.com/clxmente/NBA-Players-API"}>
+                  <a>
+                  <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
+                    <div className="flex items-center justify-between">
+                      <p className='font-bold text-lg font-mono text-gray-200'>NBA Players API</p>
+                      <div className="flex items-center">
+                        <div className="pr-4 sm:pr-7"><SiMongodb size={"20"} color="white" /></div>
+                        <div className="pr-4 sm:pr-7"><SiFastapi size={"20"} color="white" /></div>
+                        <FaPython size={"20"} color={"white"}/>
+                      </div>
                     </div>
+                    <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>API built with FastAPI to retrieve NBA player information.</p>
                   </div>
-                  <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>API built with FastAPI to retrieve NBA player information.</p>
-                </div>
-                <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
-                  <div className="flex items-center justify-between">
-                    <p className='font-bold text-lg font-mono text-gray-200'>This Website</p>
-                    <div className="flex items-center">
-                      <div className="pr-7"><FaReact size={"20"} color={"white"} /></div>
-                      <SiTailwindcss size={"20"} color={"white"} />
+                  </a>
+                </Link>
+                {/* END NBA Players API */}
+                {/* Personal Website */}
+                <Link href={"https://github.com/clxmente/nextjs-solorio.dev"}>
+                  <a>
+                  <div className="border-2 border-solid border-black bg-black rounded-md px-7 py-7 shadow-lg hover:scale-105 hover:transition-all hover:ease-out hover:duration-500 transition-all ease-out duration-500">
+                    <div className="flex items-center justify-between">
+                      <p className='font-bold text-lg font-mono text-gray-200'>This Website</p>
+                      <div className="flex items-center">
+                        <div className="pr-7"><FaReact size={"20"} color={"white"} /></div>
+                        <SiTailwindcss size={"20"} color={"white"} />
+                      </div>
                     </div>
+                    <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>This website was built with next.js, react, and tailwindcss.</p>
                   </div>
-                  <p className='text-sm font-mono text-neutral-500 pt-4 text_flicker'>This website was built with NextJS, React, and Tailwind.</p>
-                </div>
+                  </a>
+                </Link>
+                {/* END Personal Website */}
               </div>
             </div>
           </div>
